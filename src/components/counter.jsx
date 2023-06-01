9e630c2fe83985bdf178e44a0c68c2b52f6bd597
seb-art <kimanzikims300@gmail.com>
2023-06-01 16:35:38 +0300

    counter app in react

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,27 +2,46 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
+    count: 0,
+    tags: [],
   };
+
+  handleIncrement = () => {
+    this.setState( {count: this.state.count +1 })
+  }
+  resetCounter = () => {
+    this.setState( {count: 0 })
+  }
 
   render() {
     return (
       <>
-        <span className= {this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className = "btn btn-secondary btn-sm">Increment</button>
+        <span style = {{fontSize: 70}}className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-lg"
+        >
+          Increment
+        </button>
+        <button
+          onClick={this.resetCounter}
+          className="btn btn-primary btn-lg"
+          style={{margin: 10}}
+        >
+          Reset
+        </button>
       </>
-    ); 
+    );
   }
-
 
   formatCount = () => {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
-  }
+  };
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += (this.state.count === 0) ? "warning" : "primary";
+    classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
   }
 }
